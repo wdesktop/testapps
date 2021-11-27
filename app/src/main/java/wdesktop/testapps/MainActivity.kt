bf162aa4a8acbac5ff9e1d60a510c72000d876f7
wdesktop <max-nl@mail.ru>
2021-11-27 19:06:52 +0300

    2021.11.27 - 5dz

--- app/src/main/java/wdesktop/testapps/MainActivity.kt
+++ app/src/main/java/wdesktop/testapps/MainActivity.kt
@@ -2,10 +2,40 @@ package wdesktop.testapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate - В песчаных степях аравийской земли")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart - Три гордые пальмы высоко росли")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume - Родник между ними из почвы бесплодной,")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause - Журча, пробивался волною холодной,")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop - Хранимый, под сенью зеленых листов,")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy - От знойных лучей и летучих песков.")
     }
 }
